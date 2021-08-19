4d3599e8d68d48b53950bdd79b1ec93ea24307e5
FelipeFFS93 <felipe_freiresilva@yahoo.com.br>
2021-08-19 01:04:34 -0300

    atualização

---
+++ Fundamentos/assets/js/script.js
@@ -0,0 +1,17 @@
+/*
+Case Sensitive
+
+por tag: getElementByTagName()
+por id: getElementById()
+por nome: getElementsByName()
+por classe: getElementsByClassName()
+por seletor: querySelector()
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+
+nome.style.width = "100%"
+email.style.width = "100%"
+assunto.style.width = "100%"
